ada33a20794bf9bcbdb30f93189dd76b25ebf2bc
Junkyu Park <flowershy@unionpos.co.kr>
2022-09-22 14:11:06 +0900

    Kotlin study chap 3

--- app/src/main/java/com/example/sample/KotlinPractice.kt
+++ app/src/main/java/com/example/sample/KotlinPractice.kt
@@ -15,7 +15,6 @@ val square : (Int) -> (Int) = {number -> number * number}
 val nameAge =  {name : String, age : Int -> "My name is ${name}, I'm ${age}"}
 
 fun main() {
-
     println(square(12))
     println(nameAge("박준규", 35))
     println(square(5))
@@ -31,4 +30,39 @@ fun main() {
     // 반환값이 없거나 매개변수가 하나만 있을 경우
     val greet : () -> Unit
     val square : (Int) -> Int = { x -> x * x}
+
+    val a = "joyce said"
+    val b = "mac said"
+
+    println(a.pizzaIsGrea())
+    println(b.pizzaIsGrea())
+
+    println(extendString("ariana", 35))
+
+    println(calculateGrade(70))
+}
+
+// 확장함수
+val pizzaIsGrea : String.() -> String = {this + "Pizza is the best!"}
+
+fun extendString(name : String, age: Int) : String  {
+
+    val introduceMySelf : String.(Int) -> String = { "I am ${this} and ${it}  years old"} // 파라미터를 한 개만 가르키는 경우 it으로 생략이 가능하다
+
+    return name.introduceMySelf(age)
+}
+
+// 람다의 Return
+val calculateGrade : (Int) -> String = {
+    when(it) {
+        in 0..40 -> "fail"
+        in 41..70 -> "pass"
+        in 71..100 -> "perfect"
+        else -> "Error"
+    }
+}
+
+// 람다를 표현하는 여러가지 방법
+fun invokeLambda(lambda : (Double) -> Boolean) : Boolean {
+    return lambda(5.2343)
 }
