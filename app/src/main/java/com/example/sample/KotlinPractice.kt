e2cb6061d40f7d8e3bcb187c2349691238fb881b
Junkyu Park <flowershy@unionpos.co.kr>
2022-09-22 11:07:09 +0900

    Kotlin study chap 3

--- app/src/main/java/com/example/sample/KotlinPractice.kt
+++ app/src/main/java/com/example/sample/KotlinPractice.kt
@@ -1,12 +1,14 @@
 package com.example.sample
 
 // Lamda
-// 1. 람다식은 우리가 마치 value 처럼 다룰 수 있는 익명함수이다.
+// 1. 람다식이란 이름이 없어도 함수 역할을 하는 익명 함수 하나의 형태이다
 // 2. 메소드의 파라미터로 넘겨줄 수 있다. fun maxBy(a : Int)
 // 3. return 값으로 사용할 수 있다
+// 4. 람다식은 화살 표기법을 사용한다
 
 // 람다의 기본 정의
 // val lamdaName : Type = {argumentList -> codeBody}
+
 
 val square : (Int) -> (Int) = {number -> number * number}
 
@@ -16,5 +18,17 @@ fun main() {
 
     println(square(12))
     println(nameAge("박준규", 35))
+    println(square(5))
 
+    // 이름 없는 함수 형태
+    val multi = { x : Int , y : Int -> x * y }
+    println(multi(10, 20))
+    val multi2 : (Int, Int) -> Int = {x : Int , y : Int -> x * y }
+
+    // 람다식 안에 람다식이 있는 경우
+    val nestedLambda : () -> () -> Unit = { {println("nested")}}
+
+    // 반환값이 없거나 매개변수가 하나만 있을 경우
+    val greet : () -> Unit
+    val square : (Int) -> Int = { x -> x * x}
 }
